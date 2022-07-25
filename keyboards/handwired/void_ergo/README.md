907d9f50114ff3ac7186b7d3348471c3d9568fba
Mona Lisa <ceybers@users.noreply.github.com>
2022-07-25 12:04:00 +0200

    Updated keymap, added images

--- keyboards/handwired/void_ergo/README.md
+++ keyboards/handwired/void_ergo/README.md
@@ -1,8 +1,19 @@
 # VOID Ergo S
 
+![](images/20220724-IMG_1473.jpg)
+
 VIAL configuration for [Victor Lucachi](https://github.com/victorlucachi/qmk_firmware/tree/dev_void/keyboards/handwired/void_ergo)'s Void Ergo keyboard.
 
  - VIAL unlock is the top left key on the left hand side and the top right key on the right hand side.
  - Rotary encoders disabled because I am not using them on my build.
+ - Tri-state layer (`Layer 1` + `Layer 2` = `Layer 3`) disabled due to personal preference.
  - `config.h` fixed from 8x12 to 8×6 in order to work with VIA/VIAL. 
  - Compiled `.hex` is available in the root directory for convenience.
+ - [Super ALT↯TAB](https://docs.qmk.fm/#/feature_macros?id=super-alt%e2%86%aftab) implemented and bound to `USER00` and `USER01` for `Alt-Shift-Tab` (left) and `Alt-Tab (right)` respectively.
+ - Current `.vil` VIAL keymap stored in the `keymaps\vial` folder 
+
+## Current Keymap
+
+Still trying to figure out something that works for me.
+
+![](images/vial_keymap.png)
--- keyboards/handwired/void_ergo/keymaps/vial/keymap.c
+++ keyboards/handwired/void_ergo/keymaps/vial/keymap.c
@@ -15,60 +15,79 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _QWERTY,
-    _RAISE,
-    _LOWER,
-    _ADJUST
+enum custom_keycodes {          // Make sure have the awesome keycode ready
+  ALT_TAB_L = SAFE_RANGE,
+  ALT_TAB_R,
 };
+
+bool is_alt_tab_active = false; 
+uint16_t alt_tab_timer = 0;
+#define ALT_TAB_TIMER_MAX 2000
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_void_ergo(
+        KC_PSCR,    KC_Q,     KC_W,     KC_F,       KC_P,     KC_B,                     KC_J,       KC_L,     KC_U,       KC_Y,     KC_SCLN,  KC_MINS,
+        KC_ESC,     KC_A,     KC_R,     KC_S,       KC_T,     KC_G,                     KC_M,       KC_N,     KC_E,       KC_I,     KC_O,     KC_QUOT,
+        KC_TAB,     KC_Z,     KC_X,     KC_C,       KC_D,     KC_V,                     KC_K,       KC_H,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_PLUS,
+                    KC_LALT,  KC_LSFT,  KC_SPC,     MO(1),    OSM(MOD_LCTL),            KC_LCTL,    KC_BSPC,  KC_ENT,     KC_DEL,   KC_RGUI
+    ),
 
-/* OWERTY
- * |-----------------------------------------------------.                    .-----------------------------------------------------|
- * |KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC  |
- * |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- * |KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT |
- * |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- * |KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT |
- * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
- *                         KC_NO, KC_NO, KC_LGUI,   LOWER,  KC_SPC|  |  KC_ENT,  RAISE,  KC_RALT, KC_NO, KC_NO
- *                                       |------------------------|  |--------------------------|
- */
-[_QWERTY] = LAYOUT_void_ergo(
-    KC_TAB,     KC_Q,     KC_W,     KC_E,       KC_R,     KC_T,                     KC_Y,       KC_U,     KC_I,       KC_O,     KC_P,     KC_BSPC,
-    KC_LSFT,    KC_A,     KC_S,     KC_D,       KC_F,     KC_G,                     KC_H,       KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LCTL,    KC_Z,     KC_X,     KC_C,       KC_V,     KC_B,                     KC_N,       KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSFT,
-                KC_MPLY,  KC_LALT,  MO(_LOWER), KC_SPC,   KC_TRNS,                  KC_TRNS,    KC_ENT,   MO(_RAISE), KC_RGUI,  KC_MUTE
-),
+    [1] = LAYOUT_void_ergo(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 
-[_LOWER] = LAYOUT_void_ergo(
-    KC_ESC,     KC_EXLM,  KC_AT,    KC_HASH,    KC_DLR,   KC_PERC,                  KC_CIRC,    KC_AMPR,     KC_ASTR,    KC_LPRN,  KC_RPRN,  KC_BSPC,
-    KC_TRNS,    KC_LEFT,  KC_DOWN,  KC_UP,      KC_RIGHT, KC_TRNS,                  KC_LEFT,    KC_DOWN,     KC_UP,      KC_RIGHT, KC_TRNS,  KC_TRNS,
-    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
-                KC_TRNS,  KC_LGUI,  MO(_LOWER), KC_SPC,   KC_TRNS,                  KC_TRNS,    KC_ENT,      MO(_RAISE), KC_RALT,  KC_TRNS
-),
+    [2] = LAYOUT_void_ergo(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 
-[_RAISE] = LAYOUT_void_ergo(
-    KC_ESC,     KC_1,     KC_2,     KC_3,       KC_4,     KC_5,                     KC_6,       KC_7,     KC_8,       KC_9,     KC_0,     KC_BSPC,
-    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_MINS,    KC_EQL,   KC_LCBR,    KC_RCBR,  KC_PIPE,  KC_GRV,
-    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_UNDS,    KC_PLUS,  KC_LBRC,    KC_RBRC,  KC_BSLS,  KC_TILD,
-                KC_TRNS,  KC_LGUI,  MO(_LOWER), KC_SPC,   KC_TRNS,                  KC_TRNS,    KC_ENT,   MO(_RAISE), KC_RALT,  KC_TRNS
-),
-
-[_ADJUST] = LAYOUT_void_ergo(
-    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_F1,      KC_F2,    KC_F3,    KC_F4,      KC_F5,    KC_F6,                    KC_F7,      KC_F8,    KC_F9,      KC_F10,   KC_F11,   KC_F12,
-                KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  RESET,                    RESET,      KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS
-)
+    [3] = LAYOUT_void_ergo(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) { 
+    case USER00:
+        if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+                is_alt_tab_active = true;
+                register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read();
+            register_code(KC_LSFT);
+            tap_code(KC_TAB);
+            unregister_code(KC_LSFT);
+        } 
+        break;
+
+    case USER01:
+        if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+                is_alt_tab_active = true;
+                register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read();
+            tap_code(KC_TAB);
+        } 
+        break;
+    }
+    return true;
 }
 
-void matrix_init_user(void) {
-
+void matrix_scan_user(void) {
+    if (is_alt_tab_active) {
+        if (timer_elapsed(alt_tab_timer) > ALT_TAB_TIMER_MAX) {
+            unregister_code(KC_LALT);
+            is_alt_tab_active = false;
+        }
+    }
 }
